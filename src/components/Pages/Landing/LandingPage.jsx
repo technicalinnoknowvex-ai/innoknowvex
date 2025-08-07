73dafeb011a3a80db1fdb331a21bfe3c1731e8d7
AnshumanRana <223841303+buildspacedev@users.noreply.github.com>
2025-08-07 14:03:03 +0530

    Testing: star animation

--- src/components/Pages/Landing/LandingPage.jsx
+++ src/components/Pages/Landing/LandingPage.jsx
@@ -1,20 +1,59 @@
-import React from "react";
+'use client'
+import React, { useEffect, useRef } from "react";
 import landingStyles from "./styles/landing.module.scss";
 import Navbar from "../Navbar/Navbar";
+import Image from "next/image";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+if (typeof window !== "undefined") {
+  gsap.registerPlugin(ScrollTrigger);
+}
 const LandingPage = () => {
+  const starRef = useRef(null);
+  useEffect(() => {
+    if (!starRef.current) {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          scrub: 1,
+          pin: true,
+          trigger: starRef.current,
+          start: "50% 50%",
+          endTrigger: starRef.current,
+          end: "bottom 50%",
+        },
+      });
+
+      tl.to(starRef.current, {
+        rotateZ: 900,
+      });
+    }
+  }
+    , []);
   return (
     <div className={landingStyles.landing}>
-      <Navbar/>
+      <Navbar />
+
+      <Image
+        src="/images/Soft Star.svg"
+        width={95.5}
+        height={105}
+        alt="Soft Star"
+      />
+
       <p className={landingStyles.landing__text}>
-        Learn What
+        Transforming Aspirations
         <br />
-        Matters.
+        into <br />
+        Achievements
         <br />
-        Master What
-        <br />
-        Counts.
+        <Image
+          src="/images/Soft Star.svg"
+          width={95.5}
+          height={105}
+          alt="Soft Star"
+        />
       </p>
-    </div>
+      </div>
   );
 };
 
