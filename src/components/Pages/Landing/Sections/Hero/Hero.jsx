08a6cf5cd4c7fdc83130655fe9d15b756d8ef378
Praveen Lohar <praveenlohar.in@gmail.com>
2025-08-12 09:39:47 +0530

    added new font: oldschool grotesk

--- src/components/Pages/Landing/Sections/Hero/Hero.jsx
+++ src/components/Pages/Landing/Sections/Hero/Hero.jsx
@@ -8,6 +8,16 @@ const Hero = () => {
       <p className={heroStyles.heroWrapper__headingMiddle}>A B C D E F</p>
       <br />
       <p className={heroStyles.heroWrapper__headingBottom}>a b c d e f</p>
+      <p className={heroStyles.heroWrapper__para}>
+        Innoknowvex is a cutting-edge EdTech platform designed to seamlessly
+        connect students with internships, professional training, career
+        development, and expert mentorship. Our mission is to bridge the gap
+        between academic education and industry requirements by providing
+        students with access to industry-relevant programs hands-on training,
+        and specialized mentorship. Through a structured, expert-driven
+        approach, we empower aspiring professionals with the practical skills
+        and industry insights necessary to excel in their chosen fields.
+      </p>
     </section>
   );
 };
