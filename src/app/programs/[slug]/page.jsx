c283d5675243835ff70cd94bb5a4fc25576d75ce
Praveen Lohar <praveenlohar.in@gmail.com>
2025-08-27 19:54:07 +0530

    test commit

--- src/app/programs/[slug]/page.jsx
+++ src/app/programs/[slug]/page.jsx
@@ -5,8 +5,6 @@ import { programs } from "@/data/programs";
 const page = async ({ params }) => {
   const { slug } = await params;
   const program = await programs[slug];
-  console.log("🚀 ~ program:", program);
-
   return <ProgramDetailsPage program={program} />;
 };
 
