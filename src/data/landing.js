346a8ca1aeae3868e3fbf5d9863700b234ae1e3e
AnshumanRana <223841303+buildspacedev@users.noreply.github.com>
2025-11-11 10:11:47 +0530

    wc:cloudnary

--- src/data/landing.js
+++ src/data/landing.js
@@ -19,7 +19,7 @@ export const landingPageData = {
     para: `Innoknowvex is a cutting-edge EdTech platform designed to seamlessly connect students with internships, professional training, career development, and expert mentorship. Our mission is to bridge the gap between academic education and industry requirements by providing students with access to industry-relevant programs hands-on training, and specialized mentorship. Through a structured, expert-driven approach, we empower aspiring professionals with the practical skills and industry insights necessary to excel in their chosen fields.`,
     images: [
       "https://hfolrvqgjjontjmmaigh.supabase.co/storage/v1/object/public/Innoknowvex%20website%20content/About%20Us%20Images/Landing/about_us_cell_10%20(1).jpg",
-      "https://hfolrvqgjjontjmmaigh.supabase.co/storage/v1/object/public/Innoknowvex%20website%20content/About%20Us%20Images/Landing/about_us_cell_30%20(1).jpg",
+      "https://res.cloudinary.com/dquwagna6/image/upload/v1762236074/about_us_cell_30_1_hha7az.jpg",
       "https://hfolrvqgjjontjmmaigh.supabase.co/storage/v1/object/public/Innoknowvex%20website%20content/About%20Us%20Images/Landing/about_us_cell_40%20(1).jpg",
       "https://hfolrvqgjjontjmmaigh.supabase.co/storage/v1/object/public/Innoknowvex%20website%20content/About%20Us%20Images/Landing/about_us_cell_40%20(1).jpg",
       "https://hfolrvqgjjontjmmaigh.supabase.co/storage/v1/object/public/Innoknowvex%20website%20content/About%20Us%20Images/Landing/about_us_cell_90%20(1).jpg",
