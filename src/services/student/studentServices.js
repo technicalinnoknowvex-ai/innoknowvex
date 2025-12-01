ddc4a1d0fb8510045544020f356f2589b35ab98e
AnshumanRana <223841303+buildspacedev@users.noreply.github.com>
2025-12-01 22:48:43 +0530

    wc:localhost fix check

--- src/services/student/studentServices.js
+++ src/services/student/studentServices.js
@@ -1,5 +1,5 @@
 const BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:4000";
+  process.env.NEXT_PUBLIC_API_BASE_URL ;
 
 export async function getStudent(studentId) {
   try {
