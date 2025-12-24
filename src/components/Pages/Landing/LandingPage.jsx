144aa366570280d0926c63e3f0b62dea29c599e8
AnshumanRana <anshumanrana1404@gmail.com>
2025-12-24 22:51:29 +0530

    feat: carousel added for annocuments

--- src/components/Pages/Landing/LandingPage.jsx
+++ src/components/Pages/Landing/LandingPage.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import dynamic from "next/dynamic";
 import landingStyles from "./styles/landing.module.scss";
 import SmoothScroller from "@/components/Layouts/SmoothScroller";
+import Annoucements from "./Annoucements/Annoucements";
 
 const Hero = dynamic(() => import("./Sections/Hero/Hero"));
 const AboutUs = dynamic(() => import("./Sections/AboutUs/AboutUs"));
@@ -21,6 +22,7 @@ const LandingPage = () => {
     <SmoothScroller>
       <div className={landingStyles.landing}>
         <Hero />
+        <Annoucements/>
         <AboutUs />
         <Programs />
         <WhyChooseUs />
