d4d8f36fbc0783f647a11e2cb2dfda5ffb05d0b5
Anshuman Rana <anshumanrana1404@gmail.com>
2026-02-23 14:18:12 +0530

    wc: cursor removed

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
         />
         <CursorProvider>
           <ScrollProvider>
-            <Cursor />
+            {/* <Cursor /> */}
             <div className="app-root-container">{children}</div>
           </ScrollProvider>
         </CursorProvider>
